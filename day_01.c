d32604e4f444a1b96ba17f18661a058107443fc7
ZR Emon (EmoMech) <140493256+zremon2434@users.noreply.github.com>
2024-02-25 19:15:02 +0600

    Add files via upload

---
+++ day_01.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+// Author:Emon
+
+// this is single line comments
+/*this is multiple line comments*/
+
+int main()
+{
+
+    // Here are some data types
+    int numb = 4;
+    float pi = 3.14;
+    char star = '*';
+
+    printf("Hello world \n"); // here we use '\n' to get a new line
+    printf("Hello world \n");
+
+    printf("Value of pie is %f\n", pi); // here we use cases
+
+    return 0;
+}
