e7d50961a11b4b10c599e505cf466634854bd5f7
ZR Emon (EmoMech) <140493256+zremon2434@users.noreply.github.com>
2024-03-03 01:43:12 +0600

    Add files via upload

---
+++ prac3(EmoMech).c
@@ -0,0 +1,16 @@
+// Author:ZR Emon
+#include <stdio.h>
+int main()
+{
+
+    int age_num = 18;
+    float pie_value = 3.14;
+    int mathtest_num; // for input my test number
+
+    printf("Yahh broo!! Now i am %d \n", age_num);
+    printf("Ohh i remember the value of pie is %f \n", pie_value);
+
+    printf("Please emon tell me your math test number "); // for input my test number
+    scanf("%d", &mathtest_num);
+    printf("Not bad huhh %d \n", mathtest_num);
+}
